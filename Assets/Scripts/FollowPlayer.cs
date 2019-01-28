03a8b7514fa995df326e115475d4753d3038bcf9
CSIYI <siyi.cai@sjsu.edu>
2019-01-27 22:09:15 -0800

    cubethon game

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform player;
+    //set of three numbers, easy to manage x,y,z
+    public Vector3 offset;
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    // This is a reference to the Rigidbody component called "rb"
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewayForce = 500f;
+
+    public float scale;
+
+    private void Update()
+    {
+        Time.timeScale = scale;
+    }
+    // We mark this as "Fixed Update" because we are using 
+    // it to mess wtih physics
+
+    void FixedUpdate () {
+
+        //Add a forwardd force
+        rb.AddForce(0, 0, forwardForce*Time.deltaTime); // Add a force of 2000 on the z-axis
+        if(Input.GetKey("d")){
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if(rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
